b057ec34fa521edbfe6d27221879e5234ad6831a
Hungryhunter96 <ankur96xyz@gmail.com>
2019-10-19 00:20:16 +0530

    Create paytm_config.js

---
+++ paytm/paytm_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  paytm_config: {
+		MID: 'copYkQ20143736243713',
+		WEBSITE: 'DEFAULT',
+    CHANNEL_ID: 'WAP',
+    INDUSTRY_TYPE_ID: 'Retail',
+    MERCHANT_KEY : 'uYGpxYW9LM9G7IBH'
+	}
+}
